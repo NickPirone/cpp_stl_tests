2a4549377fca47115f1576a663a59cf9c3aa9108
Nick P <npirone@gmail.com>
2018-06-09 10:28:30 -0500

    merged main.cpp multi-threading into master

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -2,7 +2,7 @@ rm -r Output
 mkdir -p Output
 c++ CPP_Testing/main.cpp -o CPP_Testing/main -w -std=c++11
 cd CPP_Testing
-#./main
+./main
 #cd ..
 #cd PythonApp
 #python3 Grapher.py ../Output/Vector_Random0to100000.csv
